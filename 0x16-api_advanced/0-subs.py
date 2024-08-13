56372929d4c864f939faead5ed0109e876cbd5d5
Adeniyi Olukayode <niyisatun@yahoo.com>
2024-08-13 22:38:34 +0100

    Update 0-subs.py
    api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,7 +5,6 @@ number of subscribers for a given subreddit
 
 from requests import get
 
-
 def number_of_subscribers(subreddit):
     """
     function that queries the Reddit API and returns the number of subscribers
