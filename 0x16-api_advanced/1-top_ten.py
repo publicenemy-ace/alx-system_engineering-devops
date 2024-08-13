f0341fb13c7170d0794162630f46a04fb61dc21d
Adeniyi Olukayode <niyisatun@yahoo.com>
2024-08-13 22:44:19 +0100

    api update

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,7 +6,6 @@ prints the titles of the first 10 hot posts listed for a given subreddit
 
 from requests import get
 
-
 def top_ten(subreddit):
     """
     function that queries the Reddit API and prints the titles of the first
